88b8accb3613c31325e48fc250abd6807bc0dc2a
Mcronk <mpc3ea@virginia.edu>
2016-02-28 00:54:19 -0500

    Updated views

--- project4501_exp/urls.py
+++ project4501_exp/urls.py
@@ -3,24 +3,12 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = patterns('',
-    # Examples:
+
     url(r'^home/$',views.home),
-    #url(r'^home/(?P<class_id>[0-9]+)/(?P<info_id>[0-9]+)/$', views.home),
 
     url(r'^product/$',views.product),
     url(r'^product/(?P<course_pk>[0-9]+)/$', views.product),
 
-#    url(r'^admin/', include(admin.site.urls)),
 )
 
 
-#product page
-#course and course info 
-
-#homepage. 
-#course and course info.  
-
-#
-#
-#
-#
--- project4501_exp/views.py
+++ project4501_exp/views.py
@@ -1,23 +1,41 @@
 import urllib.request
 import urllib.parse
+import requests
 import json
 from django.http import JsonResponse
 
 
 #api/v1/home
 def home(request):
-		resp_json = {}
-		#req = urllib.request.Request('CoursePlaceholder.url', class_id = class_id)
-		#resp_json["course"] = urllib.request.urlopen(req)
-		#req = urllib.request.Request('infoPlaceholder.url', info_id = info_id)
-		resp_json["info"] = "test"
-		print(resp_json)
-		return JsonResponse(resp_json)
+		req = requests.get('http://models-api:8000/api/v1/courses/'+course_pk)
+		course = json.loads(req.txt)
+		data = { 'course' : course }
+
+		return JsonResponse(data, safe=False)
 
 
 #api/v1/product
 def product(request, course_pk = ' '):
-		req = urllib.request.Request('http://models-api:8000/api/v1/courses/'+course_pk)
-		resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-		resp = json.loads(resp_json)
-		return JsonResponse(resp, safe=False)
+		#req = urllib.request.Request('http://models-api:8000/api/v1/courses/'+course_pk)
+		course_req = requests.get('http://models-api:8000/api/v1/courses/'+course_pk)
+		
+		tutor_pk = course_req.headers.get('tutor')
+
+		tutor_req = requests.get('http://models-api:8000/api/v1/courses/'+ tutor_pk)
+		
+		course = json.loads(course.txt)
+		tutor = json.loads(tutor_req.txt)
+
+		data = { 'course' : course, 'tutor' : tutor }
+
+		#deserialize
+		courses = 
+
+		return JsonResponse(data, safe=False)
+
+		#request course via pk
+		#recieve pk and all course stuff.
+		# parse out tutor PK and then get tutor
+		# throw into one json object and return.  
+
+
