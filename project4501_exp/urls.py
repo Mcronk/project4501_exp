7d70c009fe115b06ed2fcd13052936073ceb596c
Johnson <johnson.wuyou@gmail.com>
2016-02-29 15:12:04 -0500

    reformat code, add comments, change service name to be consistent with other layers

--- project4501_exp/urls.py
+++ project4501_exp/urls.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = patterns('',
-
-    url(r'^home/$',views.home),
     url(r'^courses/$',views.courses),
     url(r'^course/$',views.course),
     url(r'^course/(?P<course_pk>[0-9]+)/$', views.course),
-
 )
-
 
--- project4501_exp/views.py
+++ project4501_exp/views.py
@@ -5,23 +5,17 @@ import json
 from django.http import HttpResponse, JsonResponse
 
 
-#api/v1/home
-def home(request):
-	req = requests.get('http://models-api:8000/api/v1/course/'+course_pk)
-	course = json.loads(req.txt)
-	data = { 'course' : course }
-	return JsonResponse(data, safe=False)
-
 #Course + Tutor: get information of a list of courses
 def courses(request):
 	course_req = requests.get('http://models-api:8000/api/v1/course/')
 	course_data = json.loads(course_req.text)
-	course_list = []
+	courses_list = []
 	for d in course_data:
 		course = {}
 		course['course_pk'] = d['pk']
 		fields = d['fields']
 		course['course_name'] = fields['name']
+		course['course_description'] = fields['description']
 		tutor_pk = fields['tutor']
 
 		user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
@@ -29,8 +23,9 @@ def courses(request):
 		for d in user_data:
 			fields = d['fields']
 			course['course_tutor'] = fields['name']
-		course_list.append(course)
-	return JsonResponse(course_list, safe=False)
+		courses_list.append(course)
+	#return a list dictionary (each dictionary is a course)
+	return JsonResponse(courses_list, safe=False)
 	
 
 #Course + Tutor: get information of a course
@@ -40,14 +35,15 @@ def course(request, course_pk = ''):
 	for d in course_data:
 		fields = d['fields']
 		course_name = fields['name']
-		price = fields['price']
-		description = fields['description']
+		course_price = fields['price']
+		course_description = fields['description']
 		tutor_pk = fields['tutor']
 	user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
 	user_data = json.loads(user_req.text)
 	for d in user_data:
 		fields = d['fields']
 		tutor_name = fields['name']
-	return JsonResponse({'name':course_name, 'price':price, 'description':description,'tutor':tutor_name}, safe=False)
+		tutor_description = fields['description']
+	return JsonResponse({'course_name':course_name, 'course_price':course_price, 'course_description':course_description,'tutor_name':tutor_name, 'tutor_description':tutor_description}, safe=False)
 
 
