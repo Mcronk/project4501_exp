40af3fc4cdc1c5a8f7c54f48997f52025eb7cd03
Johnson <johnson.wuyou@gmail.com>
2016-03-21 01:39:21 -0400

    add response checking for old views

--- project4501_exp/views.py
+++ project4501_exp/views.py
@@ -5,53 +5,13 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-#Course + Tutor: get information of a list of courses
-def courses(request):
-	course_req = requests.get('http://models-api:8000/api/v1/course/')
-	course_data = json.loads(course_req.text)
-	courses_list = []
-	for d in course_data:
-		course = {}
-		course['course_pk'] = d['pk']
-		fields = d['fields']
-		course['course_name'] = fields['name']
-		course['course_description'] = fields['description']
-		tutor_pk = fields['tutor']
-		user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
-		user_data = json.loads(user_req.text)
-		user = user_data['resp']
-		course['course_tutor'] = user['name']
-		courses_list.append(course)
-	#return a list dictionary (each dictionary is a course)
-	return JsonResponse(courses_list, safe=False)
-	
-#Course + Tutor: get information of a course
-def course(request, course_pk = ''):
-	course_req = requests.get('http://models-api:8000/api/v1/course/'+course_pk)	
-	course_data = json.loads(course_req.text)
-	course = course_data['resp']
-	tutor_pk = course['tutor']
-	user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
-	user_data = json.loads(user_req.text)
-	user = user_data['resp']
-	data = {'course_name':course['name'], 'course_price':course['price'], 'course_description':course['description'],'tutor_name':user['name'], 'tutor_description':user['description']}
-	return _success_response(request, data)
-
 #User: Create and return token
 @csrf_exempt
 def create_account(request):
-	# data = request.POST
-	# auth_req = requests.post('http://models-api:8000/api/v1/user/')
-	# token_data = json.loads(token_data.text)#should return the auth Token to be put in cookies.
-	# return JsonResponse({'token_data': token_data}, safe=False)
 	if request.method == 'POST':
 		data = request.POST
 		if not data:
 			return _error_response(request, "Failed.  No data received")
-		# try:
-		# 	data['grade'] = 0
-		# except:
-		# 	return _error_response(request, "Failed!")
 		response = requests.post('http://models-api:8000/api/v1/user/', data = data)
 		resp_data = json.loads(response.text)
 		if not resp_data or not resp_data['work']:
@@ -67,7 +27,6 @@ def login(request):
 		if not data:
 			return JsonResponse({'fail': "no POST data received"}, safe=False)
 		response = requests.post('http://models-api:8000/api/v1/authenticator/login/', data = data)
-		#login_resp = json.loads(login_resp.text)#should return true if login successful or throw an error
 		resp_data = json.loads(response.text)
 		if not resp_data or not resp_data['work']:
 		  	# couldn't log them in, send them back to login page with error
@@ -115,9 +74,6 @@ def create_course(request):
 			return _error_response(request, resp_data['msg'])
 		#get user_pk
 		tutor_pk = resp_data['resp']['tutor']
-		# if (username == "false")
-		# 	return _error_response(request, "Failed. Authenticator does not match username")
-		# listing = request.POST
 		del data['authenticator']
 		data['tutor'] = tutor_pk
 		data['popularity'] = 0
@@ -127,7 +83,41 @@ def create_course(request):
 			return _error_response(request, resp_data['msg'])
 		return _success_response(request, {'course_pk': resp_data['resp']['course_pk']})
 	return _error_response(request, "Failed. Use post")
+
+#Course + Tutor: get information of a list of courses
+def courses(request):
+	course_req = requests.get('http://models-api:8000/api/v1/course/')
+	course_data = json.loads(course_req.text)
+	courses_list = []
+	for d in course_data:
+		course = {}
+		course['course_pk'] = d['pk']
+		fields = d['fields']
+		course['course_name'] = fields['name']
+		course['course_description'] = fields['description']
+		tutor_pk = fields['tutor']
+		user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
+		user_data = json.loads(user_req.text)
+		user = user_data['resp']
+		course['course_tutor'] = user['name']
+		courses_list.append(course)
+	#return a list dictionary (each dictionary is a course)
+	return JsonResponse(courses_list, safe=False)
 	
+#Course + Tutor: get information of a course
+def course(request, course_pk = ''):
+	course_req = requests.get('http://models-api:8000/api/v1/course/'+course_pk)	
+	course_data = json.loads(course_req.text)
+	if not course_data or not course_data['work']:
+			return _error_response(request, course_data['msg'])
+	course = course_data['resp']
+	tutor_pk = course['tutor']
+	user_req = requests.get('http://models-api:8000/api/v1/user/'+str(tutor_pk))
+	user_data = json.loads(user_req.text)
+	user = user_data['resp']
+	data = {'course_name':course['name'], 'course_price':course['price'], 'course_description':course['description'],'tutor_name':user['name'], 'tutor_description':user['description']}
+	return _success_response(request, data)
+
 
 def _error_response(request, error_msg):
 	return JsonResponse({'work': False, 'msg': error_msg}, safe=False)
